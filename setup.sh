889292a5163f85578cb0f8d3547083433f83abec
Gangadhar-08 <84614291+Gangadhar-08@users.noreply.github.com>
2025-09-01 11:01:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,3 +54,4 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
+#kubeadm token create --print-join-command
